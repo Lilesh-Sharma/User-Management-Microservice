1eac045305feba8c558383fb7bea39351f250da9
Lilesh Sharma <lileshsharma1001@gmail.com>
2025-05-01 14:35:59 +0530

    First Commit

---
+++ src/test/java/com/example/user_management_microservice/UserManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_management_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
